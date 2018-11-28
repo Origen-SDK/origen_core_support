166bacadb2d748de5f41739385ade1e9a69cff5b
Stephen McGinty <stephen.mcginty@nxp.com>
2018-11-28 19:47:52 +0000

    Added part attribute test cases

---
+++ app/parts/nvm/attributes.rb
@@ -0,0 +1,9 @@
+@address_width = 16
+
+@data_width = 32
+
+@has_feature_x = true
+
+@has_feature_y = false
+
+@has_feature_z = false
---
+++ app/parts/nvm/derivatives/nvm_sub/attributes.rb
@@ -0,0 +1,5 @@
+@address_width = 12
+
+@has_feature_x = false
+
+@has_feature_z = true
