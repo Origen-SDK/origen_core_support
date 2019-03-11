666f5d0254299b268d99de00d1936ff05ca4dcd7
Stephen McGinty <stephen.mcginty@nxp.com>
2019-03-11 09:49:22 +0000

    Remove more part references

--- app/lib/origen_core_support/my_sub_block.rb
+++ app/lib/origen_core_support/my_sub_block.rb
@@ -4,7 +4,7 @@ module OrigenCoreSupport
 
     def initialize(options = {})
       if name == :my_sub_block_1
-        load_part 'sub_block_test/block1'
+        load_model 'sub_block_test/block1'
       end
     end
   end
