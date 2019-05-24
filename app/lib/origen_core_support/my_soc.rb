7f4e3520084b0eb15c660b5bf341fb5226ae9728
Stephen McGinty <stephen.mcginty@nxp.com>
2019-05-24 11:16:18 +0100

    Rename app/models to app/blocks

--- app/lib/origen_core_support/my_soc.rb
+++ app/lib/origen_core_support/my_soc.rb
@@ -4,7 +4,7 @@ module OrigenCoreSupport
 
     def initialize(options = {})
       sub_block :my_sub_block_1, class_name: 'MySubBlock'
-      sub_block :my_sub_block_2, class_name: 'MySubBlock', load_model: 'sub_block_test/block2'
+      sub_block :my_sub_block_2, class_name: 'MySubBlock', load_block: 'sub_block_test/block2'
     end
   end
 end
